f9b0258fb59b898e130db0b0023b0b33debd03a7
Atish889 <singhkabir05185@gmail.com>
2024-03-19 11:43:16 +0530

    react two basic folder

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>hello world | RAJ KUMAR</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Chai aur code react with vite | RK</h1>
+  )
+}
+
+export default App
